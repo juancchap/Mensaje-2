2786c936bbd15f633e59950b90497b59ff87372d
juancchap <156474242+juancchap@users.noreply.github.com>
2024-01-15 08:20:51 -0600

    Java Primer ejercicio

---
+++ src/mensaje2/Mensaje2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mensaje2;
+
+/**
+ *
+ * @author JUAN
+ */
+public class Mensaje2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Municipio: Atizapan");
+        System.out.println("Estado: Mexico");
+        System.out.println("Edad: 27");
+        
+    }
+    
+}
